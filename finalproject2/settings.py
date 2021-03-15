ddd2fc114078409bb5a23ce8811f9d88b494b3b2
root999 <cagriiikilic@gmail.com>
2021-03-16 00:02:38 +0300

    key changed

--- finalproject2/settings.py
+++ finalproject2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from key import key
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wl7*#*vz_1x#pfc$3g+gawtu&%^^-1o=x8m86nu!q6ims#3d^_'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
